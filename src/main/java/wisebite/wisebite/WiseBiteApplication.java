69bb55191d0bbf70e118a9f4f6897e08d89f072c
Mark van Alst <mark.van.alst@hva.nl>
2024-02-27 10:21:44 +0100

    Initial commit

---
+++ src/main/java/wisebite/wisebite/WiseBiteApplication.java
@@ -0,0 +1,13 @@
+package wisebite.wisebite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseBiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiseBiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wisebite/wisebite/WiseBiteApplicationTests.java
@@ -0,0 +1,13 @@
+package wisebite.wisebite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseBiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
