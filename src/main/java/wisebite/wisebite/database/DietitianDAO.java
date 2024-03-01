909756f6be02afc16f15644a80bfabfae920ed5c
Katarzyna Jarosz <kat.jarosz.deja@hva.nl>
2024-03-01 13:21:29 +0100

    test

--- src/main/java/wisebite/wisebite/database/DietitianDAO.java
+++ src/main/java/wisebite/wisebite/database/DietitianDAO.java
@@ -10,37 +10,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-@Repository
+
 public class DietitianDAO {
-    JdbcTemplate jdbcTemplate;
-    @Autowired
-    public DietitianDAO(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-    public List<Client> findClientsByDietitianUsername(String dietitianUsername) {
-        String sql = "SELECT u.* FROM user u " +
-                "LEFT JOIN client c ON u.username = c.username " +
-                "WHERE c.dietitian_username = ?";
 
-        return jdbcTemplate.query(sql, new ClientDAO.clientWrapmapper, dietitianUsername);
-    }
-
-    public List<User> findCoachesByDietitianUsername(String dietitianUsername) {
-        String sql = "SELECT u.* FROM user u " +
-                "INNER JOIN coach c ON u.username = c.username " +
-                "WHERE c.dietitian_username = ?";
-
-        return jdbcTemplate.query(sql, new UserDAO.UserRowMapper(), dietitianUsername);
-    }
-
-    public void storeDietitian(Dietitian dietitian) {
-        String sql = "Insert into dietitian values (?);";
-        jdbcTemplate.update(sql, dietitian.getUsername());
-    }
     public void updateDietitian(){
         //TODO
     }
-    public void deleteDietitian(){
+    public void deleteDietitian() {
         //TODO
     }
 
