e80e45ecf99e26cd07a52a9dc63b6ed9c171a4dd
Devina Hameka <devina.hameka@hva.nl>
2024-03-03 17:44:41 +0100

    created findByClientAndDate query in DailyTaskDAO.java

--- src/main/java/wisebite/wisebite/database/DailyTaskDAO.java
+++ src/main/java/wisebite/wisebite/database/DailyTaskDAO.java
@@ -1,11 +1,22 @@
 package wisebite.wisebite.database;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import wisebite.wisebite.model.DailyTask;
+
+import java.util.Date;
+import java.util.List;
 
 public class DailyTaskDAO {
+
     JdbcTemplate jdbcTemplate;
 
     public DailyTaskDAO(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
+
+    public List<DailyTask> findByClientAndDate(String clientUsername, Date date){
+        String sql = "SELECT * FROM DailyTask WHERE client = ? AND date = ?";
+        return jdbcTemplate.query(sql, new Object[]{clientUsername,date}, new DailyTaskMapper());
+    }
+
 }
