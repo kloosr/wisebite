5dad0cbd636cc6b482b2d9d152763209bfe04fd5
Devina Hameka <devina.hameka@hva.nl>
2024-03-03 18:40:06 +0100

    created findByClientAndDate method in DailyTaskRepository

--- src/main/java/wisebite/wisebite/model/DailyTask.java
+++ src/main/java/wisebite/wisebite/model/DailyTask.java
@@ -1,10 +1,22 @@
 package wisebite.wisebite.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
 public class DailyTask {
-    private final int id;
+    // 'final' removed. Assuming that id is meant to be a database-generated value (e.g., auto-incrementing),
+    // it should not be final, because it will be set by JPA (Java Persistence API) when the entity is persisted,
+    // not by your application code when the object is created.
+
+    private int id;
+    @Setter
     Plan plan;
+    @Setter
     Diet diet;
+    @Setter
     Workout workout;
+    @Setter
     private boolean dailyGoal;
 
     public DailyTask(Plan plan, Diet diet, Workout workout, boolean dailyGoal){
@@ -15,45 +27,13 @@ public class DailyTask {
         this.dailyGoal = dailyGoal;
     }
 
+    public DailyTask() {
+    }
+
 
     public boolean setDailyGoal(){
         //TODO
         return true;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public Plan getPlan() {
-        return plan;
-    }
-
-    public void setPlan(Plan plan) {
-        this.plan = plan;
-    }
-
-    public Diet getDiet() {
-        return diet;
-    }
-
-    public void setDiet(Diet diet) {
-        this.diet = diet;
-    }
-
-    public Workout getWorkout() {
-        return workout;
-    }
-
-    public void setWorkout(Workout workout) {
-        this.workout = workout;
-    }
-
-    public boolean isDailyGoal() {
-        return dailyGoal;
-    }
-
-    public void setDailyGoal(boolean dailyGoal) {
-        this.dailyGoal = dailyGoal;
-    }
 }
--- src/main/java/wisebite/wisebite/repository/DailyTaskRepository.java
+++ src/main/java/wisebite/wisebite/repository/DailyTaskRepository.java
@@ -1,4 +1,14 @@
 package wisebite.wisebite.repository;
 
+import org.springframework.stereotype.Repository;
+import wisebite.wisebite.database.DailyTaskDAO;
+import wisebite.wisebite.model.DailyTask;
+
+import java.util.Date;
+import java.util.List;
+@Repository
 public class DailyTaskRepository {
+    public List<DailyTask> findByClientAndDate (String clientUsername, Date date){
+        return DailyTaskDAO.findByClientAndDate(clientUsername, date);
+    }
 }
