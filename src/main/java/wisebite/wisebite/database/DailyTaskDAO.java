d64b7f2a71087e28b1ae2dad17eddbed5ce373b4
Devina Hameka <devina.hameka@hva.nl>
2024-03-03 17:33:47 +0100

    created a constructor in DailyTaskDAO.java

--- src/main/java/wisebite/wisebite/database/DailyTaskDAO.java
+++ src/main/java/wisebite/wisebite/database/DailyTaskDAO.java
@@ -1,4 +1,11 @@
 package wisebite.wisebite.database;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class DailyTaskDAO {
+    JdbcTemplate jdbcTemplate;
+
+    public DailyTaskDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 }
