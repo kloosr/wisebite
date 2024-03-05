f89c3e6cc4fc6987e473e43ca60fbd2d34b3953f
Mark van Alst <mark.van.alst@hva.nl>
2024-03-05 15:36:30 +0100

    Adds AddAndFindUser Test

--- src/test/java/wisebite/wisebite/service/AdminServiceUnitTest.java
+++ src/test/java/wisebite/wisebite/service/AdminServiceUnitTest.java
@@ -1,4 +1,39 @@
 package wisebite.wisebite.service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import wisebite.wisebite.model.Admin;
+import wisebite.wisebite.model.User;
+import wisebite.wisebite.repository.AdminRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@ComponentScan("wisebite.wisebite")
 public class AdminServiceUnitTest {
+
+    private AdminRepository adminRepository;
+    String username = "Testuser";
+
+    @Autowired
+    public  AdminServiceUnitTest(AdminRepository adminRepository) {
+        this.adminRepository = adminRepository;
+    }
+    @BeforeEach
+    public void start() {
+        adminRepository.deleteUser(username);
+    }
+    @Test
+    public void addAndFindUser() {
+        User testUser1 = new Admin(username, "123456789", "Mark", "van","Alst");
+        adminRepository.createUser(testUser1);
+        User testUser2 = adminRepository.getUserByUsername(username).get();
+
+        assertEquals(username, testUser2.getUsername());
+        assertEquals(testUser1.getLastName(), testUser2.getLastName());
+
+    }
 }
