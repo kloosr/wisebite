5a7d626a13143b80a328393362628f51b2185bb8
Katarzyna Jarosz <kat.jarosz.deja@hva.nl>
2024-03-06 12:40:09 +0100

    ClientDTO - class created

--- src/main/java/wisebite/wisebite/dto/ClientDTO.java
+++ src/main/java/wisebite/wisebite/dto/ClientDTO.java
@@ -1,4 +1,23 @@
 package wisebite.wisebite.dto;
 
+import java.util.Date;
+
 public class ClientDTO {
+    private String firstName;
+    private String infix;
+    private String lastName;
+    private double weight;
+    private int height;
+    private Date startDate;
+    public ClientDTO (){}
+
+    public ClientDTO(String firstName, String infix, String lastName, double weight, int height, Date startDate) {
+        this.firstName = firstName;
+        this.infix = infix;
+        this.lastName = lastName;
+        this.weight = weight;
+        this.height = height;
+        this.startDate = startDate;
+    }
 }
+
