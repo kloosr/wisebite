cf9a62fe0de1ffe8a92f666eed7ad183540bb67a
Mark van Alst <mark.van.alst@hva.nl>
2024-03-06 15:32:38 +0100

    Rebuilds missing ClientInfo class?!

---
+++ src/main/java/wisebite/wisebite/model/ClientInfo.java
@@ -0,0 +1,38 @@
+package wisebite.wisebite.model;
+
+import wisebite.wisebite.service.validation.ValidHeight;
+import wisebite.wisebite.service.validation.ValidWeight;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+public class ClientInfo extends UserInfo {
+    @ValidHeight
+    private final Integer height;
+
+    @ValidWeight
+    private final Double weight;
+
+    public ClientInfo(String username, String password, String firstName, String infix, String lastName, Integer height, Double weight) {
+        super(username, password, firstName, infix, lastName);
+        this.height = height;
+        this.weight = weight;
+        this.setUserType(UserTypeEnum.CLIENT);
+    }
+    @Override
+    public Client convertToUser() {
+        Date currentDate = Date.valueOf(LocalDate.now());
+        return new Client(this.getUsername(), this.getPassword(), this.getFirstName(),
+                this.getInfix(), this.getLastName(), this.getWeight(), this.getHeight(),
+                currentDate);
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+}
