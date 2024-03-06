9dd1c54927e8ffe3b63f6321a7155d08466b4ee0
Mark Fijn <markfijn@hva.nl>
2024-03-06 10:40:25 +0100

    /overview/username now asks for a token and checks if the user has the role "coach" before being able to see al client information

--- src/main/java/wisebite/wisebite/controller/CoachController.java
+++ src/main/java/wisebite/wisebite/controller/CoachController.java
@@ -1,10 +1,12 @@
 package wisebite.wisebite.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import wisebite.wisebite.dto.PlanDTO;
 import wisebite.wisebite.model.Plan;
+import wisebite.wisebite.service.AuthenticationService;
 import wisebite.wisebite.service.PlanningService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +22,17 @@ import java.util.List;
 public class CoachController {
  private final UserManagementService userManagementService;
  private final PlanningService planningService;
+ private final AuthenticationService authenticationService;
 
     @Autowired
-    public CoachController(PlanningService planningService, UserManagementService userManagementService) {
+    public CoachController(PlanningService planningService, UserManagementService userManagementService, AuthenticationService authenticationService) {
         this.planningService = planningService;
         this.userManagementService = userManagementService;
+        this.authenticationService = authenticationService;
+    }
+    @GetMapping("/token")
+    public ResponseEntity<String> getToken() {
+        return ResponseEntity.ok(authenticationService.login());
     }
 
     // Endpoint om een overzicht van alle cliënten voor de coach op te halen
@@ -33,14 +41,19 @@ public class CoachController {
         List<Client> clients = userManagementService.findAllByCoach(coachUsername);
         return ResponseEntity.ok(clients);
     }
-    @GetMapping("/plan/{username}")
-    private ResponseEntity<PlanDTO> getPlanByClient(@PathVariable String username) {
-        Plan plan = planningService.getPlanByClient(username);
-        if (plan == null) {
-            return ResponseEntity.notFound().build();
+    @GetMapping("/overview/{username}")
+    private ResponseEntity<PlanDTO> getPlanByClient(@PathVariable String username,
+                                                    @RequestHeader String jwtToken) {
+        if (authenticationService.hasAcces(jwtToken)) {
+            Plan plan = planningService.getPlanByClient(username);
+            if (plan == null) {
+                return ResponseEntity.notFound().build();
+            } else {
+                PlanDTO planDTO = PlanDTO.convertToDTO(plan);
+                return ResponseEntity.ok(planDTO);
+            }
         } else {
-            PlanDTO planDTO = PlanDTO.convertToDTO( plan );
-            return ResponseEntity.ok(planDTO);
+            return ResponseEntity.badRequest().build();
         }
     }
 }
--- src/main/java/wisebite/wisebite/service/AuthenticationService.java
+++ src/main/java/wisebite/wisebite/service/AuthenticationService.java
@@ -1,19 +1,55 @@
 package wisebite.wisebite.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import io.jsonwebtoken.Jwt;
 import wisebite.wisebite.database.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import wisebite.wisebite.model.User;
 
+import java.nio.charset.StandardCharsets;
+import java.sql.Time;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Base64;
+import java.util.Date;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class AuthenticationService {
     private final UserDAO userDAO;
+    Algorithm algorithm = Algorithm.HMAC256("wisebite");
     @Autowired
     public AuthenticationService (UserDAO userDAO) {this.userDAO = userDAO;}
     public Optional<User> findByUsername (String username) {
         return userDAO.findByUsername(username);
     }
 
+    public String login() {
+        String jwtToken = JWT.create()
+                .withIssuer("wisebite")
+                .withSubject("wisebitedetails")
+                .withClaim("role", "coach")
+                .withIssuedAt(Instant.now())
+                .withExpiresAt(Instant.now().plusSeconds(600))
+                .sign(algorithm);
+        return jwtToken;
+    }
+
+    public boolean hasAcces (String jwtToken) {
+        JWTVerifier jwtVerifier = JWT.require(algorithm).withIssuer("wisebite").build();
+        DecodedJWT decodedJWT = jwtVerifier.verify(jwtToken);
+        if (decodedJWT.getClaim("role").asString().equals("coach")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
